7946a8ebd5e2ccc6d49037d118cf7ae02e58484b
Lucasng777 <163145504+Lucasng777@users.noreply.github.com>
2024-08-21 10:56:39 -0300

    a

--- excercício.js
+++ excercício.js
@@ -1,1 +1,13 @@
-fun
+function Exercicio1(){
+    let atvi
+    while(true){
+        atvi = prompt ("digite uma nota de 0 a 10") ;
+        atvi = parseInt (atvi);
+        if (atvi <0|| atvi>10){
+            alert("valor invalido");
+        } else {
+            break;
+        }
+    }
+}
+Exercicio1();
